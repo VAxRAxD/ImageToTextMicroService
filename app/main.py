3ee279ce79300a795344fe07479f424fb6d37c3f
Varad Prabhu <varadp763@gmail.com>
2023-06-01 20:08:20 +0530

    FastAPI and Pytest

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import pathlib
+from fastapi import FastAPI,Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+BASE_DIR=pathlib.Path(__file__).parent
+
+app=FastAPI()
+templates=Jinja2Templates(directory=str(BASE_DIR/"templates"))
+
+@app.get("/", response_class=HTMLResponse)
+def home(request:Request):
+    return templates.TemplateResponse("home.html",{"request":request})
