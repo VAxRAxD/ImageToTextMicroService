6e406b0b348b3898a986128cd33c8ded229a3c06
Varad Prabhu <varadp763@gmail.com>
2023-06-06 16:18:02 +0530

    Removed necessary functionalities

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-import pathlib,io,uuid
-from fastapi import FastAPI,Request,Depends,HTTPException,File,UploadFile,Header 
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse,FileResponse
+import pathlib,io,pytesseract
+from fastapi import FastAPI,Depends,HTTPException,File,UploadFile,Header
 from pydantic import BaseSettings
 from functools import lru_cache
 from PIL import Image
 
 class Settings(BaseSettings):
+    app_auth_token: str
     debug: bool = False
-    echo_active: bool = False
     class Config:
         env_file=".env"
 
@@ -17,30 +15,25 @@ def get_settings():
     return Settings()
 
 DEBUG=get_settings().debug
-
 BASE_DIR=pathlib.Path(__file__).parent
-UPLOAD_DIR=BASE_DIR/"uploads"
-UPLOAD_DIR.mkdir(exist_ok=True)
 
 app=FastAPI()
-templates=Jinja2Templates(directory=str(BASE_DIR/"templates"))
 
-@app.get("/", response_class=HTMLResponse)
-def home(request:Request, settings:Settings=Depends(get_settings)):
-    print(DEBUG)
-    return templates.TemplateResponse("home.html",{"request":request})
+def verify_auth(authorization=Header(None),settings:Settings=Depends(get_settings)):
+    if settings.debug:
+        return
+    if authorization is None:
+        raise HTTPException(detail="Unauthorized Response", status_code=401)
+    if authorization != settings.app_auth_token:
+        raise HTTPException(detail="Unauthorized Response", status_code=401)
 
-@app.post("/img-echo/",response_class=FileResponse)
-async def imgEcho(file:UploadFile=File(...), settings:Settings=Depends(get_settings)):
-    if not settings.echo_active:
-        raise HTTPException(detail="Invalid Endpoint", status_code=400)
+@app.post("/convert/")
+async def imgToText(file:UploadFile=File(...), authorization=Header(None), settings:Settings=Depends(get_settings)):
+    verify_auth(authorization, settings)
     bytes_str=io.BytesIO(await file.read())
     try:
         img=Image.open(bytes_str)
     except:
         raise HTTPException(detail="Invalid Image", status_code=400)
-    filename=pathlib.Path(file.filename)
-    fileext=filename.suffix
-    dest=UPLOAD_DIR/f"{uuid.uuid1()}{fileext}"
-    img.save(dest)
-    return dest
+    text=str(pytesseract.image_to_string(img)).replace('\n',' ').replace('.','.\n')
+    return {"text":text}
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,30 +1,17 @@
-import shutil,io
 from fastapi.testclient import TestClient
-from . main import app,BASE_DIR,UPLOAD_DIR
-from PIL import Image,ImageChops
+from . main import app,BASE_DIR,get_settings
+from PIL import Image
 
 client=TestClient(app)
 
-def test_home():
-    response=client.get("/")
-    assert response.status_code==200
-    assert  "text/html" in response.headers['content-type']
-
-def test_imgEcho():
-    for path in (BASE_DIR/"images").glob("*"):
+def test_imgToText():
+    for path in (BASE_DIR/"test_images").glob("*"):
         try:
             img=Image.open(path)
         except:
             img=None
-        response=client.post("/img-echo/",files={"file":open(path,'rb')})
+        response=client.post("/convert/",files={"file":open(path,'rb')},headers={"Authorization":get_settings().app_auth_token})
         if img is not None:
             assert response.status_code==200
-            assert response.status_code == 200
-            r_stream = io.BytesIO(response.content)
-            echo_img = Image.open(r_stream)
-            difference = ImageChops.difference(echo_img, img).getbbox()
-            assert difference is None
         else:
             assert response.status_code==400
-    shutil.rmtree(UPLOAD_DIR)
-    
